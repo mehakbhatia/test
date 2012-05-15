b25f8a2be3269ae4d3b359dfefc4d25bbb6aadc8
mehakbhatia <mehakbhatia22@gmail.com>
2012-05-15 17:07:02 +0530

    Fix bug - memory leak

--- hello world.m
+++ hello world.m
@@ -15,6 +15,7 @@ int main (int argc, const char * argv[]) {
     NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
 	Hello *instance1 = [[Hello alloc] init];
 	[instance1 print];
+	[instance release];
    	[pool drain];
     	return 0;
 }
